7a2b411246df744eaab3e7319c2a6703772e6878
partydragen <adrian-kili@hotmail.no>
2016-06-07 16:01:03 +0200

    Update language.php

--- addons/BanAppeal/language.php
+++ addons/BanAppeal/language.php
@@ -10,7 +10,6 @@
 $banappeal_language = array(
 	'ban_appeal' => 'Ban Appeal',
 	'ban_appeal_icon' => '<i class="fa fa-users"></i> ', // Icon to display before the text in the navbar
-	'comment_error' => 'Please ensure your comment is between 2 and 2048 characters long.',
 	'viewing_open_ban_appeal' => 'Viewing <span class="label label-info">open</span> ban appeal. Change to <a href="/mod/banappeal/?view=accepted"><span class="label label-success">accepted</span></a> or <a href="/mod/banappeal/?view=declined"><span class="label label-danger">declined</span></a>.',
 	'viewing_accepted_ban_appeal' => 'Viewing <span class="label label-success">accepted</span> ban appeal. Change to <a href="/mod/banappeal/"><span class="label label-info">open</span></a> or <a href="/mod/banappeal/?view=declined"><span class="label label-danger">declined</span></a>.',
 	'viewing_declined_ban_appeal' => 'Viewing <span class="label label-danger">declined</span> ban appeal. Change to <a href="/mod/banappeal/"><span class="label label-info">open</span></a> or <a href="/mod/banappeal/?view=accepted"><span class="label label-success">accepted</span></a>.',
@@ -24,6 +23,9 @@ $banappeal_language = array(
 	'decline' => 'Decline',
 	'new_ban_appeal_submitted_alert' => 'New Ban Appeal submitted by {x}', // Don't replace "{x}"
 	'new_comment' => 'New comment',
+	'comment_error' => 'Please ensure your comment is between 2 and 2048 characters long.',
+	'comments' => 'Comments',
+	'comment_added' => 'Comment added.',
 	'submit' => 'Submit',
 	'ban_appeal_submitted' => 'Ban Appeal submitted successfully.',
 	'ban_appeal_already_submitted' => 'You\'ve already submitted an ban appeal. Please wait until it is complete before submitting another.',
