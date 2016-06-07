0a70c75e26183736d496da5e92aec654d0dfc26b
partydragen <adrian-kili@hotmail.no>
2016-06-07 15:17:24 +0200

    Add files via upload

--- addons/BanAppeal/language.php
+++ addons/BanAppeal/language.php
@@ -8,6 +8,25 @@
 // Language file for "banappeal" addon
 
 $banappeal_language = array(
-	'banappeal' => 'BanAppeal',
-	'banappeal_icon' => '<i class="fa fa-users"></i> ', // Icon to display before the text in the navbar
+	'ban_appeal' => 'Ban Appeal',
+	'ban_appeal_icon' => '<i class="fa fa-users"></i> ', // Icon to display before the text in the navbar
+	'comment_error' => 'Please ensure your comment is between 2 and 2048 characters long.',
+	'viewing_open_ban_appeal' => 'Viewing <span class="label label-info">open</span> ban appeal. Change to <a href="/mod/banappeal/?view=accepted"><span class="label label-success">accepted</span></a> or <a href="/mod/banappeal/?view=declined"><span class="label label-danger">declined</span></a>.',
+	'viewing_accepted_ban_appeal' => 'Viewing <span class="label label-success">accepted</span> ban appeal. Change to <a href="/mod/banappeal/"><span class="label label-info">open</span></a> or <a href="/mod/banappeal/?view=declined"><span class="label label-danger">declined</span></a>.',
+	'viewing_declined_ban_appeal' => 'Viewing <span class="label label-danger">declined</span> ban appeal. Change to <a href="/mod/banappeal/"><span class="label label-info">open</span></a> or <a href="/mod/banappeal/?view=accepted"><span class="label label-success">accepted</span></a>.',
+	'time_applied' => 'Time Applied',
+	'no_ban_appeal' => 'No Ban Appeal in this category',
+	'viewing_ban_appeal_from' => 'Viewing Ban Appeal from {x}', // Don't replace "{x}"
+	'open' => 'Open',
+	'accepted' => 'Accepted',
+	'declined' => 'Declined',
+	'accept' => 'Accept',
+	'decline' => 'Decline',
+	'new_ban_appeal_submitted_alert' => 'New Ban Appeal submitted by {x}', // Don't replace "{x}"
+	'new_comment' => 'New comment',
+	'submit' => 'Submit',
+	'ban_appeal_submitted' => 'Ban Appeal submitted successfully.',
+	'ban_appeal_already_submitted' => 'You\'ve already submitted an ban appeal. Please wait until it is complete before submitting another.',
+	'ban_appeal_accepted' => 'Your staff ban appeal has been accepted.',
+	'ban_appeal_rejected' => 'Your staff ban appeal has been rejected.'
 );
